b525c6ad8662a5469863775b3b5a2d1c870abd90
victoriaghoos <ghoosvictoria@gmail.com>
2026-09-18 00:24:46 +0200

    Remove unused assets and set up initial types and data files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import './App.css'
 
 function App() {
@@ -11,9 +8,9 @@ function App() {
     <>
       <section id="center">
         <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+          <img className="base" width="170" height="179" alt="" />
+          <img className="framework" alt="React logo" />
+          <img className="vite" alt="Vite logo" />
         </div>
         <div>
           <h1>Get started</h1>
@@ -42,13 +39,13 @@ function App() {
           <ul>
             <li>
               <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
+                <img className="logo" alt="" />
                 Explore Vite
               </a>
             </li>
             <li>
               <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
+                <img className="button-icon" alt="" />
                 Learn more
               </a>
             </li>
